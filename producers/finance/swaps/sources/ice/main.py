4f0fd772489b13a28e3c9533901f85efa272d8bc
jackgray <jgrayau@gmail.com>
2024-08-29 18:54:33 -0400

    successsfully streaming in parallel for ICE reports

--- producers/finance/swaps/sources/ice/main.py
+++ producers/finance/swaps/sources/ice/main.py
@@ -1,10 +1,35 @@
-from spotlight_utils.main import get_token, fetch_csv, create_table_from_dict, df_to_clickhouse, generate_datestrings
+import pandas as pd
+import argparse
+import asyncio
+from spotlight_utils.main import get_token, create_table_from_dict, generate_datestrings, fetch_with_adaptive_concurrency
 from config import ice_source_schema as schema_dict
 
 
-def transform_df(df: pd.DataFrame, url: str) -> pd.DataFrame:
 
-    df['_RecordID'] = df['Dissemination identifier'] + 
+async def main(start_date='20240101', end_date='today', schema_dict=schema_dict, token=None, table_name='Swaps_ICE_source'):
+    ''' Grab records from ICE US SDR in parallel for a given range of dates '''
+    
+    if token is None: # Get token if it wasn't provided
+        token = await get_token("https://tradevault.ice.com/tvsec/ticker/webpi/getToken")
+        print("Got token ", token)
+    datestrings = generate_datestrings(start_date=start_date, end_date=end_date)
+    urls = [f'https://tradevault.ice.com/tvsec/ticker/webpi/exportTicks?date={datestring[:4]}-{datestring[4:6]}-{datestring[6:8]}' for datestring in datestrings]
+
+
+    create_table_from_dict(schema_dict=schema_dict, table_name=table_name, key_col='_Record_ID')   # Create staging table
+
+    await fetch_with_adaptive_concurrency(
+        urls=urls,
+        token=token,
+        table_name=table_name,
+        chunk_size=100000,
+        transform_func=transform_df
+    )
+
+def transform_df(df: pd.DataFrame, url: str) -> pd.DataFrame:
+    ''' Pass this into fetch pipeline to transform dataframes mid-stream '''
+
+    df['_Record_ID'] = df['Dissemination identifier'] + \
         df['Event timestamp'].astype(str).str.replace(r'[-:TZ]', '', regex=True)
     df.columns = (
         df.columns
@@ -21,29 +46,14 @@ def transform_df(df: pd.DataFrame, url: str) -> pd.DataFrame:
     return df
 
 
-async def main(start_date='20240101', end_date='today', schema_dict=schema_dict, token=None, table_name='Swaps_ICE_source', ch_settings=ch_settings):
-    ''' Grab record from ICE US SDR for a given date '''
-    
-    if token is None: # Get token if it wasn't provided
-        token = await get_token("https://tradevault.ice.com/tvsec/ticker/webpi/getToken")
-
-    datestrings = generate_datestrings(start_date='20240101', end_date='yesterday')
-
-    urls=[]   # turn dates into urls to fetch in parallel
-    for datestring in datestrings:
-        url_datestring = '-'.join([datestring[:4], datestring[4:6], datestring[6:8]])   # Convert date into format url uses
-        url = f'https://tradevault.ice.com/tvsec/ticker/webpi/exportTicks?date={url_datestring}'
-        urls.append(url)
-
-    await fetch_with_adaptive_concurrency(
-        urls=urls,
-        token=token,
-        table_name=table_name,
-        chunk_size=100000,
-        transform_func=transform_df
-    )
-
-
-
 if __name__ == "__main__":
-    asyncio.run(main(start_date='20240101', end_date='today', schema_dict=schema_dict, token=None, table_name='Swaps_ICE_source', ch_settings=ch_settings))
+
+    parser = argparse.ArgumentParser(description="Fetch and process CSV data from ICE US SDR.")
+    parser.add_argument('--start_date', type=str, default='20240101', help='Start date in YYYYMMDD format')
+    parser.add_argument('--end_date', type=str, default='today', help='End date in YYYYMMDD format (or "today")')
+    parser.add_argument('--table_name', type=str, default='Swaps_ICE_source', help='Table name for ClickHouse')
+    parser.add_argument('--token', type=str, help='Authentication token for API requests')
+
+    args = parser.parse_args()
+
+    asyncio.run(main(start_date=args.start_date, end_date=args.end_date, token=args.token, table_name=args.table_name))
